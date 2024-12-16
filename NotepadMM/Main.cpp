2c5bca57708be0dcc07dde17c75427b109d2b697
Distilled-Water <danielcharlesworth26@gmail.com>
2024-12-16 16:32:41 +0000

    Added typing word wrapping

--- NotepadMM/Main.cpp
+++ NotepadMM/Main.cpp
@@ -1,14 +1,14 @@
 #include <SDL.h>
 #include <SDL_ttf.h>
 #include <iostream>
-#include <cstring>
+#include <string>
+#include <vector>
 
 
-void cleanup(SDL_Window* window, SDL_Renderer* renderer, TTF_Font* font, SDL_Texture* texture) {
-	if (window) SDL_DestroyWindow;
-	if (renderer) SDL_DestroyRenderer;
-	if (font) TTF_CloseFont;
-	if (texture) SDL_DestroyTexture;
+void cleanup(SDL_Window* window, SDL_Renderer* renderer, TTF_Font* font) {
+	if (window) SDL_DestroyWindow(window);
+	if (renderer) SDL_DestroyRenderer(renderer);
+	if (font) TTF_CloseFont(font);
 
 	TTF_Quit();
 	SDL_Quit();
@@ -17,22 +17,40 @@ void cleanup(SDL_Window* window, SDL_Renderer* renderer, TTF_Font* font, SDL_Tex
 int main(int argc, char* argv[]) {
 
 	SDL_Init(SDL_INIT_EVERYTHING);
-	TTF_Init();
+	
 
 	// Creates a window
 	SDL_Window* window = SDL_CreateWindow("Notepad--", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
-	if (!window) { std::cout << "Failed to create window! Error:" << SDL_GetError() << std::endl; }
+	if (!window) { 
+		std::cout << "Failed to create window! Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return -1;
+	}
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+	if (!renderer) {
+		std::cout << "Failed to create renderer! Error: " << SDL_GetError() << std::endl;
+		cleanup(window, nullptr, nullptr);
+		return -1;
+	}
+
+	TTF_Init();
 
 	// Create a font and colour
 	TTF_Font* font = TTF_OpenFont("arial.ttf", 24);
-	if (!font) std::cout << "Failed to load font! Error:" << TTF_GetError() << std::endl;
+	if (!font) {
+		std::cout << "Failed to load font! Error:" << TTF_GetError() << std::endl;
+		cleanup(window, renderer, nullptr);
+		return -1;
+	}
 	SDL_Color green = { 0, 0, 255, 255 };
 	SDL_Texture* textTexture = nullptr;
 
 
 	// MAIN LOOP
-	char text[100] = "Hello";
+	std::vector<std::string> lines = {""};
+	int lineSpacing = 0;
+	int maxWidth = 570;
+
 	bool running = true;
 	SDL_Event e;
 	while (running) {
@@ -45,34 +63,63 @@ int main(int argc, char* argv[]) {
 			case SDL_KEYDOWN:
 				// If any key is pressed, convert that to a string and concat that with initial text
 				if (e.key.keysym.sym >= SDLK_SPACE && e.key.keysym.sym <= SDLK_z) {
-					char keyPressed[2] = { static_cast<char>(e.key.keysym.sym), '\0' };
-					strcat_s(text, sizeof(text), keyPressed);
+					// appends letter to last line
+					char keyPressed = static_cast<char>(e.key.keysym.sym);
+					lines.back() += keyPressed;
+
+					// create new line if width>maxWidth
+					int textWidth;
+					int textHeight;
+					TTF_SizeText(font, lines.back().c_str(), &textWidth, &textHeight);
+
+					if (textWidth > maxWidth) {
+						lines.push_back("");
+					}
+				}
+				else if (e.key.keysym.sym == SDLK_BACKSPACE)
+				{
+					if (lines.back().size() >= 1) {
+						lines.back().pop_back();
+					}
+					else if (lines.size() >= 2) {
+						lines.pop_back();
+					}
+
 				}
 			default:
 				break;
 			}
 		}
 
-		// Renders text, then converts that into a surface
-		SDL_Surface* textSurface = TTF_RenderText_Blended(font, text, green);
-		if (!textSurface) { std::cout << "Failed to render text! Error:" << TTF_GetError() << std::endl; return -1; }
-		SDL_Texture* textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
 
 		// draws background
 		SDL_SetRenderDrawColor(renderer, 188, 255, 255, 255);
 		SDL_RenderClear(renderer);
 
-		// Renders Text into a box
-		SDL_Rect textRect = { 10, 10, textSurface->w, textSurface->h };
-		SDL_RenderCopy(renderer, textTexture, NULL, &textRect);
+		// draws char on each line of vector
+		int yOffset = 10;
+		for (const std::string& line : lines) {
 
-		SDL_DestroyTexture(textTexture);
-		textTexture = nullptr;
+			// Renders text, then converts that into a surface
+			SDL_Surface* textSurface = TTF_RenderText_Blended(font, line.c_str(), green);
+			if (textSurface) {
+				SDL_Texture* textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
+
+				// Renders Text into a box
+				SDL_Rect textRect = { 10, yOffset, textSurface->w, textSurface->h };
+				SDL_RenderCopy(renderer, textTexture, NULL, &textRect);
+
+				yOffset += textSurface->h + lineSpacing;
+
+				SDL_DestroyTexture(textTexture);
+			}
+			SDL_FreeSurface(textSurface);
+		}
 
 		// updates screen
 		SDL_RenderPresent(renderer);
 	}
 
-	cleanup(window, renderer, font, textTexture);
+	cleanup(window, renderer, font);
 	return 0;
 }
